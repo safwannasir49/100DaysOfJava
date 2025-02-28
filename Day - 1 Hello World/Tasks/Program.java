2435a844f11fa699154beabec58e622bbf042945
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-02-28 13:32:35 +0530

    Create Program.java

---
+++ Day - 1 Hello World/Tasks/Program.java
@@ -0,0 +1,11 @@
+// This is a single-line comment
+/*
+   This is a multi-line comment.
+   It explains the purpose of the program.
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Printing output
+    }
+}
