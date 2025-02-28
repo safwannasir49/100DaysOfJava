6ebc94ea9d434cdb70a835deee77006baf316d84
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-02-28 13:26:26 +0530

    Create VariableDemo.java

---
+++ Day - 2 Varibales and Datatypes/VariableDemo/VariableDemo.java
@@ -0,0 +1,11 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int myInt = 25;
+        double myDouble = 3.14;
+        String myString = "Hello, Java!";
+
+        System.out.println("Integer value: " + myInt);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("String value: " + myString);
+    }
+}
